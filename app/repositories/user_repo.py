6f3c8f1c0d663fbe689e3239268159104d38382f
salmanbashar1009 <salmanbashar101@gmail.com>
2026-04-04 14:04:25 +0600

    auth services added

--- app/repositories/user_repo.py
+++ app/repositories/user_repo.py
@@ -7,18 +7,18 @@ class UserRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-        async def get_by_email(self,email:str) -> Optional[User]:
-            statement = select(User).where(User.email == email)
-            result= await self.session.exec(statement)
-            return result.one_or_none()
+    async def get_by_email(self,email:str) -> Optional[User]:
+        statement = select(User).where(User.email == email)
+        result= await self.session.exec(statement)
+        return result.one_or_none()
         
-        async def create(self, user: User) -> User:
-            self.session.add(user)
-            try:
-                await self.session.commit()
-                await self.session.refresh()
-                return user
-            except Exception:
-                await self.session.rollback()
-                raise 
+    async def create(self, user: User) -> User:
+        self.session.add(user)
+        try:
+            await self.session.commit()
+            await self.session.refresh()
+            return user
+        except Exception:
+            await self.session.rollback()
+            raise 
             
---
+++ app/services/auth_service.py
@@ -0,0 +1,26 @@
+from app.repositories.user_repo import UserRepository
+from app.models.user import User
+from app.core.security import get_hased_password, verify_password, create_access_token
+
+class AuthService:
+    def __init__(self,user_repo: UserRepository):
+        self.user_repo = user_repo
+
+    async def register_user(self, email: str, password: str):
+        existing_user  = await self.user_repo.get_by_email(email)
+        if existing_user:
+            raise ValueError("Email already registered")
+        
+        hashed_pwd = get_hased_password(password)
+        user = User(email=email, hashed_password=hashed_pwd)
+        return await self.user_repo.create(user)
+    
+    async def login_user(self, email:str, password:str):
+        user = await self.user_repo.get_by_email(email)
+
+        if not user or not verify_password(password, user.hashed_password):
+            raise ValueError("Invalid credentials")
+        
+        token = create_access_token({"sub":str(user.id),"role": user.role})
+
+        return {"access_token": token, "token_type": "bearer"}
