684c3335911c12dd924b0547a6fa092f9e24ea66
salmanbashar1009 <salmanbashar101@gmail.com>
2026-04-04 15:28:23 +0600

    api dependencies added

---
+++ app/api/deps.py
@@ -0,0 +1,36 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from pwdlib import jwt, exceptions
+from sqlmodel.ext.asyncio.session import AsyncSession
+from app.core.database import get_session
+from app.core.config import settings
+from app.models.user import User
+from sqlmodel import select
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+async def get_current_user(
+        token:str = Depends(oauth2_scheme),
+        session: AsyncSession = Depends(get_session)
+)-> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials"
+    )
+
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithoms = [settings.ALGORITHM])
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+        
+    except Exception:
+        raise credentials_exception
+    
+    statement = select(User).where(User.id == user_id)
+    result = await session.exec(statement)
+    user = result.first()
+
+    if user is None:
+        raise credentials_exception
+    return user
