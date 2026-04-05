9e3ddca6c7208b7ce85bed5f753f51823c9f53c0
salmanbashar1009 <salmanbashar101@gmail.com>
2026-04-05 20:30:14 +0600

    comment schema added

--- app/repositories/task_repo.py
+++ app/repositories/task_repo.py
@@ -1,6 +1,7 @@
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlmodel import select
 from app.models.task import Task
+from app.models.comment import Comment
 import uuid
 from typing import Optional
 
@@ -27,3 +28,22 @@ class TaskRepository:
         statement = statement.offset(skip).limit(limit)
         result = await self.session.exec(statement)
         return result.all
+    
+    async def update(self,task:Task)->Task:
+        self.session.add(task)
+        await self.session.commit()
+        await self.session.refresh(task)
+        return task
+    
+
+    async def delete(self, task:Task):
+        await self.session.delete(task)
+        await self.session.commit()
+
+
+    # Comments
+    async def add_comment(self, comment: Comment) -> Comment:
+        self.session.add(comment)
+        await self.session.commit()
+        await self.session.refresh(comment)
+        return comment
